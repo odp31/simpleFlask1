782dc2b4667d16496a16cba2f9f7162a80891ce3
Olivia Polos <38380599+odp31@users.noreply.github.com>
2024-12-04 09:45:23 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,39 @@
-from flask import Flask
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
+# sample project data
+
+projects = [
+  {
+    "title" : "python web scraper",
+    "description": "scrapes data from website and stores in database",
+    "technologies": ["Python", "BeautifulSoup4"],
+    "github_link": "https.//github.com/odp31/web_scraper",
+    "live_demo": None #optional link to live dmeo 
+  },
+  {
+    "title": "machine learning model for image classification", 
+    "description": "machine learning model",
+    "technologies": ["Python", "TensorFlow", "Keras"],
+    "github_link": "https://github.com/odp31/image_classifier",
+    "live_demo": None
+  },
+]
+# route for main portfolio page
+
 @app.route('/')
-def hello_world():
-  return 'Hello, world!'
+def portfolio():
+  return render_template('portfolio.html', projects=projects)
+
+# route for specific project page
+@app.route('/project/<python web scraper>')
+def project_details(project_title):
+  for project in projects:
+    if project["title"] == project_title:
+      return render_template('project_details.html', project=project)
+  return "project not found", 404 # handle non existent project
 
 if __name__ == '__main__':
   app.run(debug=True)
+  
